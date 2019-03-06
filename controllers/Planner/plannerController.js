68611bd7b31efb7031b9044ecbe12f4e9646d7b1
dezrenmikell <dezren.mikell@yahoo.com>
2019-03-06 04:19:38 -0500

    changed plannerController to render views

--- controllers/Planner/plannerController.js
+++ controllers/Planner/plannerController.js
@@ -5,42 +5,58 @@ const plannerController = {
     index: (req, res) => {
         User.findById(req.params.userId)
             .then(() => {
-                res.send('user.events')
+                res.send(user.events)
             })
     },
     new: (req, res) => {
-        res.send("Plan New")
-    },
-    create: (req, res) => {
-        User.create({
-            email: req.body.email,
-            password: req.body.password
-        }).then(user => {
-            res.send("Plan Edit")
+        res.render('personnel/new', {
+            userId: req.params.userId
         })
     },
+    create: (req, res) => {
+        User.findById(req.params.userId)
+        .then(user =>{
+            Event.create({
+            content: req.body.content,
+            reviews: [{content: 'This is so cool'}]
+        })
+        .then(newEvent => {
+            user.events.push(newEvent)
+            user.save()
+            res.redirect(`/users/${req.params.userId}/events`)
+        })
+    })
+    
+    },
+
     show: (req, res) => {
         User.findById(req.params.userId)
            
             .then(user => {
                 res.send('Plan Show')
             })
+           
     },
     edit: (req, res) => {
         Planner.Event.findById(req.params.eventId)
         .then(event => {
-            res.send('Plan Edit')
+            res.render('planner/edit', {
+                event,
+                userId: req.params.userId,
+                eventId: req.params.eventId
+            })
         })
     },
     update: (req, res) => {
-        Event.findByIdAndUpdate(req.params.eventId, {content: req.body.content}, {new: true}).then(updatedEvent => {
-            res.redirect(`/users/${req.params.userId}/events`)
+        Planner.Event.findByIdAndUpdate(req.params.eventId, {content: req.body.content}, {new: true}).then(updatedEvent => {
+            res.redirect(`/users/${req.params.userId}/events/${req.params.eventId}`)
         })
     },
 
     delete: (req, res) => {
-        User.findByIdAndDelete(req.params.userId).then(() => {
-            res.send('Plan Delete')
+        Planner.Event.findByIdAndDelete(req.params.eventId).then(() => {
+            console.log('deleted event')
+            res.redirect(`/users/${req.params.userId}/events`)
         })
     }
 }
